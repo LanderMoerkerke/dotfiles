b50728f081a60b7a61e1bcaf97c4d457e9d681a5
Lander Moerkerke <lander.moerkerke@telenet.be>
2019-01-10 09:58:23 +0100

    Checks twice for unread

--- .config/polybar/mailcheck.sh
+++ .config/polybar/mailcheck.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
+check_unread () {
+    unread=$(find ~/.mail/*/INBOX/new | grep new/ | wc | awk '{print $1;}')
+
+    if [ "$unread" != "0" ]; then
+        echo $unread
+    else
+        echo ""
+    fi
+}
+
 # Checks mails
 echo "Syncing mail"
 ~/.config/mutt/etc/mailsync.sh > /dev/null 2>/dev/null
 
 # Checks amount of mails
-unread=$(find ~/.mail/*/INBOX/new | grep new/ | wc | awk '{print $1;}')
-
-if [ "$unread" != "0" ]; then
-    echo $unread
-else
-    echo ""
-fi
+check_unread
+sleep 20
+check_unread
