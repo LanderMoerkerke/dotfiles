a0135250a1ea34385416a94462a3361abb382791
Lander Moerkerke <lander.moerkerke@telenet.be>
2019-03-07 21:58:44 +0100

    Added highlight theme

--- .config/ranger/scope.sh
+++ .config/ranger/scope.sh
@@ -36,7 +36,7 @@ FILE_EXTENSION_LOWER=$(echo ${FILE_EXTENSION} | tr '[:upper:]' '[:lower:]')
 # Settings
 HIGHLIGHT_SIZE_MAX=262143  # 256KiB
 HIGHLIGHT_TABWIDTH=8
-HIGHLIGHT_STYLE='pablo'
+HIGHLIGHT_STYLE='wombat'
 PYGMENTIZE_STYLE='autumn'
 
 
@@ -141,7 +141,7 @@ handle_mime() {
                 local pygmentize_format='terminal'
                 local highlight_format='ansi'
             fi
-            highlight --replace-tabs="${HIGHLIGHT_TABWIDTH}" --out-format="${highlight_format}" \
+            highlight -D $HOME/.config/highlight/ --replace-tabs="${HIGHLIGHT_TABWIDTH}" --out-format="${highlight_format}" \
                 --style="${HIGHLIGHT_STYLE}" --force -- "${FILE_PATH}" && exit 5
             # pygmentize -f "${pygmentize_format}" -O "style=${PYGMENTIZE_STYLE}" -- "${FILE_PATH}" && exit 5
             exit 2;;
