1057c2d3f716fad9a10eecbf37cbc2efb433519b
Lander Moerkerke <lander.moerkerke@telenet.be>
2019-05-27 22:16:15 +0200

    Extend preview of jupyter notebooks

--- .config/lf/pv.sh
+++ .config/lf/pv.sh
@@ -6,7 +6,7 @@ case "$1" in
     *.rar) unrar l "$1";;
     *.7z) 7z l "$1";;
     *.pdf) pdftotext "$1" -;;
-    *.ipynb) jupyter nbconvert --stdout --to python "$1";;
+    *.ipynb) jupyter nbconvert --stdout --to python "$1" | sed -e '/^[ \t]*#/d' | black -q - | highlight --syntax python -O xterm256 -D /home/lander/.config/highlight/ -s rdark;;
     *.mp4) mediainfo "$1";;
     *) highlight -O xterm256 -D /home/lander/.config/highlight/ -s rdark --force -- "$1" || cat "$1";;
     # *) highlight -O ansi "$1" || cat "$1";;
