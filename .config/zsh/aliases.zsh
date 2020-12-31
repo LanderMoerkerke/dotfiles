aa587ff747148af5d2a3cf457b8934d93f3beb3b
Lander Moerkerke <lander.moerkerke@telenet.be>
2020-12-31 10:21:52 +0100

    Switch to double quotes

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -78,7 +78,7 @@ alias pacman_listpackages_size="paste <(pacman -Q | awk '{ print $1; }' | xargs 
 alias rtv='rtv --enable-media'
 alias soundcloud="scdl"
 alias we='curl wttr.in/Gent'
-alias lf='$HOME/.local/bin/lf'
+alias lf="$HOME/.local/bin/lf"
 
 # programming
 
