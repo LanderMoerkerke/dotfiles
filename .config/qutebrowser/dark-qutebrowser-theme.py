645a966e7148dc3abd3532bde4488395b2681a93
Lander Moerkerke <lander.moerkerke@telenet.be>
2021-02-20 16:29:37 +0100

    Add dark background colour

--- .config/qutebrowser/dark-qutebrowser-theme.py
+++ .config/qutebrowser/dark-qutebrowser-theme.py
@@ -333,4 +333,4 @@ c.colors.tabs.selected.odd.fg = nord['nord5']
 # Background color for webpages if unset (or empty to use the theme's
 # color)
 ## Type: QtColor
-# c.colors.webpage.bg = 'white'
+c.colors.webpage.bg = '#212121'
