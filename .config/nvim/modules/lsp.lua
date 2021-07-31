32aecdfc6afc9b563e6d8d326ea6bda425d146c5
Lander Moerkerke <lander.moerkerke@telenet.be>
2021-07-31 17:53:22 +0200

    Update LSP gutter symbols

--- .config/nvim/modules/lsp.lua
+++ .config/nvim/modules/lsp.lua
@@ -171,6 +171,14 @@ require "compe".setup {
     }
 }
 
+
+-- Change gutter signs
+local signs = { Error = " ", Warning = " ", Hint = " ", Information = " " }
+for type, icon in pairs(signs) do
+  local hl = "LspDiagnosticsSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
+
 -- -------------------------------------------
 -- LSP signature
 -- -------------------------------------------
