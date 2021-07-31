c33eb453809db4aa2c2c2149b6c95f257bbfa189
Lander Moerkerke <lander.moerkerke@telenet.be>
2021-07-31 17:54:35 +0200

    Add explicit highlighter settings of diagnostics

--- .config/nvim/modules/lsp.lua
+++ .config/nvim/modules/lsp.lua
@@ -171,6 +171,13 @@ require "compe".setup {
     }
 }
 
+-- Customizing how diagnostics are displayed
+vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = true,
+})
 
 -- Change gutter signs
 local signs = { Error = " ", Warning = " ", Hint = " ", Information = " " }
