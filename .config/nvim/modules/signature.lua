c5438c8fade12d0abe5bf09ab25a81e74fbbe6ad
Lander Moerkerke <lander.moerkerke@telenet.be>
2022-03-04 18:32:44 +0100

    Update signature

--- .config/nvim/modules/signature.lua
+++ .config/nvim/modules/signature.lua
@@ -1,9 +1,10 @@
 require("lsp_signature").on_attach(
     {
         bind = true,
-        fix_pos = true,
+        fix_pos = false,
         handler_opts = {
             border = "single"
         }
+        -- zindex = 50
     }
 )
