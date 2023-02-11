7483ca637adbd84ad1eed4d7ef6c5491b4f8fa82
Lander Moerkerke <lander.moerkerke@telenet.be>
2023-02-11 13:46:42 +0100

    Update gitsigns

--- .config/nvim/modules/gitsigns.lua
+++ .config/nvim/modules/gitsigns.lua
@@ -17,7 +17,7 @@ gitsigns.setup {
         follow_files = true
     },
     attach_to_untracked = true,
-    current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+    current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
     current_line_blame_opts = {
         virt_text = true,
         virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
@@ -50,8 +50,39 @@ gitsigns.setup {
         end
 
         -- Navigation
-        map("n", "]c", "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", {expr = true})
-        map("n", "[c", "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", {expr = true})
+        map(
+            "n",
+            "]c",
+            function()
+                if vim.wo.diff then
+                    return "]c"
+                end
+                vim.schedule(
+                    function()
+                        gs.next_hunk()
+                    end
+                )
+                return "<Ignore>"
+            end,
+            {expr = true}
+        )
+
+        map(
+            "n",
+            "[c",
+            function()
+                if vim.wo.diff then
+                    return "[c"
+                end
+                vim.schedule(
+                    function()
+                        gs.prev_hunk()
+                    end
+                )
+                return "<Ignore>"
+            end,
+            {expr = true}
+        )
 
         -- Actions
         map({"n", "v"}, "<leader>hs", ":Gitsigns stage_hunk<CR>")
