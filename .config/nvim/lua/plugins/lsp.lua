2c51fce0bdc5bced094e755b7e1deaf8db4c61a2
Lander Moerkerke <lander.moerkerke@telenet.be>
2023-12-11 08:49:40 +0100

    Disable virtual text

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -144,7 +144,7 @@ return {
                 vim.lsp.with(
                 vim.lsp.diagnostic.on_publish_diagnostics,
                 {
-                    virtual_text = true,
+                    virtual_text = false,
                     signs = true,
                     underline = true,
                     update_in_insert = true
