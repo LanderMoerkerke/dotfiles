764eec1c3793585687c1d5225b419de747d17d0f
Lander Moerkerke <lander.moerkerke@telenet.be>
2023-12-11 08:49:04 +0100

    Simplify indentline

--- .config/nvim/lua/plugins/indentline.lua
+++ .config/nvim/lua/plugins/indentline.lua
@@ -1,8 +1,6 @@
 return {
     "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
     event = "VeryLazy",
-    opts = {
-        show_current_context = true,
-        show_current_context_start = true
-    }
+    opts = {}
 }
