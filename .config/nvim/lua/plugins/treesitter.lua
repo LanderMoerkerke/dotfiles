2ee4f090fc75ce03af266c41918809c97863a910
Lander Moerkerke <lander.moerkerke@telenet.be>
2026-04-03 14:21:05 +0200

    nvim: update treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,16 +1,36 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+        "windwp/nvim-ts-autotag", "JoosepAlviste/nvim-ts-context-commentstring"
+    },
     build = ":TSUpdate",
     lazy = false,
     branch = "main",
-    opts = {
-        highlight = {
-            enable = true,
-            disable = {"csv"}
-        },
-        incremental_selection = {enable = true},
-        indent = {enable = true},
-        autotag = {enable = true},
-        context_commentstring = {enable = true, enable_autocmd = false}
-    }
+    config = function()
+        require("nvim-treesitter").setup({
+            ensure_installed = {
+                "python", "lua", "bash", "html", "css", "javascript",
+                "typescript", "json", "yaml", "toml", "markdown",
+                "markdown_inline", "vim", "vimdoc", "dap_repl"
+            }
+        })
+
+        require('nvim-ts-autotag').setup({
+            opts = {
+                enable_close = true,
+                enable_rename = true,
+                enable_close_on_slash = true
+            }
+        })
+
+        -- Highlighting is now built into Neovim, enable it via autocommand
+        vim.api.nvim_create_autocmd("FileType", {
+            callback = function()
+                -- Skip csv files
+                if vim.bo.filetype == "csv" then return end
+                pcall(vim.treesitter.start)
+            end
+        })
+    end
+
 }
