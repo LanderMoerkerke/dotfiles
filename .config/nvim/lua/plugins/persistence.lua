6ccb703cda7b157144e15a464a22d5ba266b06f3
Lander Moerkerke <lander.moerkerke@telenet.be>
2026-08-19 14:08:08 +0200

    nvim: update mapping of persistence plugin

--- .config/nvim/lua/plugins/persistence.lua
+++ .config/nvim/lua/plugins/persistence.lua
@@ -3,8 +3,8 @@ return {
     event = "BufReadPre", -- this will only start session saving when an actual file was opened
     opts = {},
     keys = {
-        {"<leader>ps", function() require("persistence").load() end, desc = "Restore session (cwd)"},
-        {"<leader>pl", function() require("persistence").load({last = true}) end, desc = "Restore last session"},
-        {"<leader>pd", function() require("persistence").stop() end, desc = "Stop session save"}
+        {"<leader>qs", function() require("persistence").load() end, desc = "Restore session (cwd)"},
+        {"<leader>ql", function() require("persistence").load({last = true}) end, desc = "Restore last session"},
+        {"<leader>qd", function() require("persistence").stop() end, desc = "Stop session save"}
     }
 }
