3a670c76d002f48b705a0a169177ae1e9885dedd
Lander Moerkerke <lander.moerkerke@telenet.be>
2026-09-15 08:54:24 +0200

    nvim: add scratch command

--- .config/nvim/lua/config/basic_commands.lua
+++ .config/nvim/lua/config/basic_commands.lua
@@ -1,2 +1,19 @@
 vim.api.nvim_create_user_command("ReverseBuffer", "g/^/m0", {})
 vim.api.nvim_create_user_command("JSONToPython", "%s/true/True/g | %s/false/False/g | %s/null/None/g", {})
+
+vim.api.nvim_create_user_command("Scratch", function(opts)
+    local ext = opts.args ~= "" and opts.args or "md"
+    local path = os.date("/tmp/scratch-%Y%m%d-%H%M%S.") .. ext
+    vim.cmd.edit(vim.fn.fnameescape(path))
+end, {
+    nargs = "?",
+    complete = function(lead)
+        local exts = {
+            "md", "json", "json5", "yaml", "yml", "toml", "http", "sql",
+            "py", "sh", "lua", "js", "ts", "xml", "csv", "txt",
+        }
+        return vim.tbl_filter(function(e)
+            return e:find("^" .. lead)
+        end, exts)
+    end,
+})
